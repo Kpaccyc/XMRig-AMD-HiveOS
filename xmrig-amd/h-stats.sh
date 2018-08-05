aef420d408363c8e2cb8324bfae171b086c4f638
Viktor Komarov <41443179+Kpaccyc@users.noreply.github.com>
2018-08-05 11:27:01 +0300

    Update h-stats.sh

--- xmrig-amd/h-stats.sh
+++ xmrig-amd/h-stats.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
-
 stats_raw=`curl --connect-timeout 2 --max-time $API_TIMEOUT --silent --noproxy '*' http://127.0.0.1:60050`
 #echo $stats_raw | jq .
 if [[ $? -ne 0 || -z $stats_raw ]]; then
 echo -e "${YELLOW}Failed to read $miner from localhost:60050${NOCOLOR}"
 else
 khs=`echo $stats_raw | jq -r '.hashrate.total[0]' | awk '{print $1/1000}'`
+local hs_units="hs"
 local ac=$(jq '.results.shares_good' <<< "$stats_raw")
 local rj=$(( $(jq '.results.shares_total' <<< "$stats_raw") - $ac ))
-stats=$(jq --argjson temp "$temp" --argjson fan "$fan" --arg ac "$ac" --arg rj "$rj"  \
-'{hs: [.hashrate.threads[][0]], algo: .algo, $temp, $fan, uptime: .connection.uptime, ar: [$ac, $rj]}' <<< "$stats_raw")
+stats=$(jq --argjson temp "$temp" --argjson fan "$fan" --arg ac "$ac" --arg rj "$rj" --arg hs_units "$hs_units" \
+'{hs: [.hashrate.threads[][0]], algo: .algo, $temp, $fan, $hs_units, uptime: .connection.uptime, ar: [$ac, $rj]}' <<< "$stats_raw")
 fi
-
