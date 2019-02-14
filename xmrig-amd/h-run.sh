b5bb51ae30ad72527554cc116e5cc741b69c1466
Viktor Komarov <41443179+Kpaccyc@users.noreply.github.com>
2019-02-14 14:22:59 +0300

    Update h-run.sh

--- xmrig-amd/h-run.sh
+++ xmrig-amd/h-run.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 cd `dirname $0`
 
-[ -t 1 ] && . colors
-
-#CUSTOM_MINER="xmrigamd"
-#. /hive-config/wallet.conf
-#[[ -z $CUSTOM_MINER ]] && echo -e "${RED}No CUSTOM_MINER is set${NOCOLOR}" && exit 1
-#. /hive/custom/$CUSTOM_MINER/h-manifest.conf
-
 . h-manifest.conf
 
-#echo $CUSTOM_MINER
-#echo $CUSTOM_LOG_BASENAME
-#echo $CUSTOM_CONFIG_FILENAME
+while true; do
+  for con in `netstat -anp | grep TIME_WAIT | grep $CUSTOM_API_PORT | awk '{print $5}'`; do
+    killcx $con lo
+  done
+  netstat -anp | grep TIME_WAIT | grep $CUSTOM_API_PORT &&
+    continue ||
+    break
+done
 
-[[ -z $CUSTOM_LOG_BASENAME ]] && echo -e "${RED}No CUSTOM_LOG_BASENAME is set${NOCOLOR}" && exit 1
-[[ -z $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}No CUSTOM_CONFIG_FILENAME is set${NOCOLOR}" && exit 1
-[[ ! -f $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}Custom config ${YELLOW}$CUSTOM_CONFIG_FILENAME${RED} is not found${NOCOLOR}" && exit 1
-CUSTOM_LOG_BASEDIR=`dirname "$CUSTOM_LOG_BASENAME"`
-[[ ! -d $CUSTOM_LOG_BASEDIR ]] && mkdir -p $CUSTOM_LOG_BASEDIR
+
+wget https://github.com/Kpaccyc/XMRig-AMD-HiveOS/blob/master/README.md
+if [ $(cat README.md | grep -i v$CUSTOM_VERSION | wc -l) -eq 0 ];
+  then
+    mv $CUSTOM_CONFIG_FILENAME /hive/miners/custom/;
+    /hive/miners/custom/custom-get https://github.com/Kpaccyc/XMRig-AMD-HiveOS/releases/download/Release/xmrigamd.tar.gz -f;
+    mv /hive/miners/custom/config.conf $CUSTOM_CONFIG_FILENAME;
+fi
+rm -f README.md
 
 if [ $(dpkg-query -W -f='${Status}' libmicrohttpd-dev 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
@@ -26,10 +28,10 @@ then
   apt-get install libmicrohttpd-dev -y;
 fi
 
-cd /hive/custom/$CUSTOM_NAME
+cd /hive/miners/custom/$CUSTOM_NAME
 export GPU_FORCE_64BIT_PTR=1
 export GPU_USE_SYNC_OBJECTS=1
 export GPU_MAX_ALLOC_PERCENT=100
 export GPU_SINGLE_ALLOC_PERCENT=100
 export GPU_MAX_HEAP_SIZE=100
-./xmrig-amd --api-port=65056 $(< /hive/custom/$CUSTOM_NAME/config.conf) $@ 2>&1 | tee $CUSTOM_LOG_BASENAME.log
+./xmrig-amd --api-port=$CUSTOM_API_PORT $(< $CUSTOM_CONFIG_FILENAME) $@ 2>&1 | tee $CUSTOM_LOG_BASENAME.log
